5413e43d420c5a21e0e58db2787bc00704926bcf
Oleg Khomyakov <noc@mail.ru>
2012-11-18 18:40:59 +0400

    add exe-singleton

--- src/ai/aw/Activator.java
+++ src/ai/aw/Activator.java
@@ -1,5 +1,9 @@
 package ai.aw;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URI;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
@@ -15,16 +19,30 @@ public class Activator implements BundleActivator {
 
     @Override
     public void start(BundleContext context) throws Exception {
+    	
+        try {  
+             ServerSocket serverSocket = new java.net.ServerSocket(50382);  
+        } catch (IOException ex) {  
+             javax.swing.JOptionPane.showMessageDialog(null, "aw уже запущен.  http://localhost:8080/ ");
+             this.stop(context);
+             context.getBundle(0).stop();
+             System.exit(0);
+             return;
+        }  
+
+        
         Dictionary<String, Object> properties = new Hashtable<String, Object>();
         properties.put(HTTP_PORT_KEY, HTTP_PORT);
         JettyConfigurator.startServer(SERVER_NAME, properties);
-        System.out.println("Server " + SERVER_NAME + " has been started");
+        //System.out.println("Server " + SERVER_NAME + " has been started");
+        URI url = new URI("http://localhost:8080/welcome?name=");
+        Desktop.getDesktop().browse(url);
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
         JettyConfigurator.stopServer(SERVER_NAME);
-        System.out.println("Server " + SERVER_NAME + " has been stoped");
+        //System.out.println("Server " + SERVER_NAME + " has been stoped");
     }
 
 }
