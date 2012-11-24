f3d52d45d1e8b5d422adcda3b0ddb2d4774bd2d0
Oleg Khomyakov <noc@mail.ru>
2012-11-25 01:36:47 +0400

    add findIP

--- src/ai/aw/Sqlite.java
+++ src/ai/aw/Sqlite.java
@@ -47,9 +47,10 @@ public class Sqlite {
 		}
 		
 	}
-
+//////  FIND IP //////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 	public String findIP(String textIP) {
-		String res="", ip, mac , timecreate, timelast;
+		String res="", ip, descr, mac , timecreate, timelast;
 		PreparedStatement st=null;
 		ResultSet rs=null;
 		
@@ -57,23 +58,40 @@ public class Sqlite {
 		
 	    try {
 	    		    	
-	    	st = con.prepareStatement ("select * from arp where ip = ?"); 
+	    	st = con.prepareStatement ("select * from ipdesc where ip = ?"); 
 	    	st.setString (1, textIP); 
-	    	
-		    rs = st.executeQuery();
-
+	    	rs = st.executeQuery();
+		    while (rs.next()) {
+		    	ip = rs.getString("ip");
+		    	descr = rs.getString("descr");
+		    	res += "IP: " + ip + "  descr: " + descr + " <br>";
+		    }
+		} 
+		catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	    finally {
+			try {
+				rs.close();
+				st.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	    
+	    try {
+	    	st = con.prepareStatement ("select * from arp where ip = ? ORDER BY timelast DESC"); 
+	    	st.setString (1, textIP); 
+	    	rs = st.executeQuery();
 		    while (rs.next()) {
 		    	ip = rs.getString("ip");
 		    	mac = rs.getString("mac");
-		    	//timecreate = rs.getString("timecreate");
-		    	//timelast = rs.getDouble("timelast");
-		    	//timelast = rs.getString("timelast");
-		    	res += "\n******************************";
-		    	res += "\nIP: " + ip;
-		    	res += "\nMAC: " + mac;
-		    	//res += "\nTimeCreate: " + timecreate;
-		    	//res += "\nTimeLast: " + timelast;
-		    	res += "\n******************************";
+		    	timelast = rs.getString("timelast");
+		    	res += "IP: " + ip + "  MAC: " + mac + "  last: " + timelast + " <br>";
+		    	
+		    	
+		    	
+		    	
 		    }
 		} 
 		catch (SQLException e1) {
@@ -110,27 +128,27 @@ $query = "SELECT * FROM arp WHERE ip = '{$ipparam}' ORDER BY timelast DESC";
 mysql_query($query);
 $result = mysql_query($query);
 while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
-printf("IP: %s  MAC: %s  last: %s <br>", $row[1], $row[3], $row[5]); 
-$query2 = "SELECT * FROM ports WHERE mac = '{$row[3]}' ORDER BY timelast DESC";
-$result2 = mysql_query($query2);
-while ($row2 = mysql_fetch_array($result2, MYSQL_NUM)) {
-printf("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ipsw: %s  port: %s  last: %s <br>", $row2[4], $row2[3], $row2[6]);
+  printf("IP: %s  MAC: %s  last: %s <br>", $row[1], $row[3], $row[5]); 
+  $query2 = "SELECT * FROM ports WHERE mac = '{$row[3]}' ORDER BY timelast DESC";
+  $result2 = mysql_query($query2);
+  while ($row2 = mysql_fetch_array($result2, MYSQL_NUM)) {
+    printf("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ipsw: %s  port: %s  last: %s <br>", $row2[4], $row2[3], $row2[6]);
 
-$query3 = "SELECT * FROM crossport WHERE ipsw = '{$row2[4]}' AND port = '{$row2[3]}'"; 
-$result3 = mysql_query($query3);
-while ($row3 = mysql_fetch_array($result3, MYSQL_NUM)) {
-printf("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Розетка: %s <br>", $row3[1]);
+    $query3 = "SELECT * FROM crossport WHERE ipsw = '{$row2[4]}' AND port = '{$row2[3]}'"; 
+    $result3 = mysql_query($query3);
+    while ($row3 = mysql_fetch_array($result3, MYSQL_NUM)) {
+      printf("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Розетка: %s <br>", $row3[1]);
 
-}
-mysql_free_result($result3);
+    }
+    mysql_free_result($result3);
 
-$query3 = "SELECT * FROM ports WHERE ipsw = '{$row2[4]}' AND port = '{$row2[3]}' ORDER BY timelast DESC";
-$result3 = mysql_query($query3);
-if(mysql_num_rows($result3) > 1) {
-while ($row3 = mysql_fetch_array($result3, MYSQL_NUM)) {
-printf("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MAC: %s  last: %s <br>", $row3[2], $row3[6]);
+    $query3 = "SELECT * FROM ports WHERE ipsw = '{$row2[4]}' AND port = '{$row2[3]}' ORDER BY timelast DESC";
+    $result3 = mysql_query($query3);
+    if(mysql_num_rows($result3) > 1) {
+      while ($row3 = mysql_fetch_array($result3, MYSQL_NUM)) {
+        printf("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MAC: %s  last: %s <br>", $row3[2], $row3[6]);
 
-}
+      }
 }
 mysql_free_result($result3);
 
